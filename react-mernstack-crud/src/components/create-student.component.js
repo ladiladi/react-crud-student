5d87b65c5edcc07dd20ee4a682f8af0a663151ba
ladiladi <38681465+ladiladi@users.noreply.github.com>
2020-08-17 13:13:35 +0300

    Frontend done, backend having errors
    Still having some MongoDB database connectivitity errors. Rollbacked database start.

---
+++ react-mernstack-crud/src/components/create-student.component.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import axios from 'axios';
+
+export default class CreateStudent extends Component {
+    constructor(props) {
+        super(props)
+
+        // Setting up functions
+        this.onChangeStudentName = this.onChangeStudentName.bind(this);
+        this.onChangeStudentEmail = this.onChangeStudentEmail.bind(this);
+        this.onChangeStudentRollno = this.onChangeStudentRollno.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        // Setting up state
+        this.state = {
+            name: '',
+            email: '',
+            rollno: ''
+        }
+    }
+
+    onChangeStudentName(e) {
+        this.setState({name: e.target.value})
+    }
+
+    onChangeStudentEmail(e) {
+        this.setState({email: e.target.value})
+    }
+
+    onSubmit(e) {
+        e.preventDefault()
+
+        const studentObject = {
+            name: this.state.name,
+            email: this.state.email,
+            rollno: this.state.rollno
+        };
+        axios.post('http://localhost:4000/students/create-students', studentObject)
+        .then(res => console.log(res.data));
+
+        this.setState({name: '', email: '', rollno: ''})
+    }
+
+    onChangeStudentRollno(e) {
+        this.setState({rollno: e.target.value})
+    }
+
+    render() {   
+      return (<div class="form-wrapper">
+          <Form>
+              <Form.Group controlId="Name">
+                  <Form.Label>Name</Form.Label>
+                  <Form.Control type="text"/>
+              </Form.Group>
+
+              <Form.Group controlId="Email">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control type="email"/>
+              </Form.Group>
+
+              <Form.Group controlId="Name">
+                  <Form.Label>Roll No</Form.Label>
+                  <Form.Control type="text"/>
+              </Form.Group>
+
+              <Button variant="danger" size="1g" block="block" type="submit">
+                  Create Student
+              </Button>
+
+          </Form>
+      </div>
+      );
+    }
+}
